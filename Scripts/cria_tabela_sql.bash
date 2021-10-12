acd8b2c196a441b284f94c2321b088d388ed0af9
ArthurMPassos <arthurmoreirap@gmail.com>
2021-10-11 22:59:22 -0300

    script creates table

--- Scripts/cria_tabela_sql.bash
+++ Scripts/cria_tabela_sql.bash
@@ -1,12 +1,33 @@
 #!/bin/bash
-echo "Creating MySQL user and database"
 USER=admin
 PASS=admin
 DATABASE=friendlybudget
 
+echo "Creating MySQL user, database and exemple user (Gabriel)"
 mysql -u root << MYSQL_SCRIPT
 CREATE DATABASE $DATABASE;
 CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASS';
 GRANT ALL PRIVILEGES ON $USER.* TO '$USER'@'localhost';
 FLUSH PRIVILEGES;
+
+
+USE $DATABASE
+CREATE SCHEMA IF NOT EXISTS friendlyBudget;
+
+USE friendlyBudget;
+
+create table USERS(
+    NAME varchar(50) NOT NULL PRIMARY KEY
+);
+
+create table TRANSACTIONS(
+    NAME varchar(50) NOT NULL,
+    FOREIGN KEY (NAME) REFERENCES USERS(NAME),
+    DATE char(10) NOT NULL,
+    VALUE float NOT NULL,
+    DESCRIPTION varchar(50)
+);
+
+INSERT INTO USERS (NAME) VALUES ('Gabriel');
+
 MYSQL_SCRIPT
